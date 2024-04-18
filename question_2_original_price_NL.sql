fd9fb87cbc0a71e0757ba71da8fc18746c868fcb
cansuizat <cicansu@gmail.com>
2024-04-18 12:17:00 +0200

    fixed typo

--- question_2_original_price_NL.sql
+++ question_2_original_price_NL.sql
@@ -1,7 +1,7 @@
 /* For this question, I decided not to use a CTE.
 The query uses an inner join to join the ad table with the event table, 
 then uses another inner join to join the event table with the event location table.
-The query filters out results for the Netherlands, adn calculates the average original price.*/
+The query filters out results for the Netherlands, and calculates the average original price.*/
 SELECT 
     AVG(original_price) AS average_original_price
 FROM 
